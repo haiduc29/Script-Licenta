b513a2e6e44022c9befb06493b7572a6bf01e272
haiduc29 <ionut.cojocaru93@gmail.com>
2024-06-24 21:32:48 +0300

    Update Test_unhide.ps1

--- Test_unhide.ps1
+++ Test_unhide.ps1
@@ -25,7 +25,7 @@ try{
 # Descarca imaginea
 $url = "https://similarpng.com/wp-content/uploadPngfree/thumbnail/2024/02/Business-Technology-Digital-High-Tech-World-Background.png"
 $outputPath = "C:\Users\ionut\Downloads\unPwned.jpg" 
-Invoke-WebRequest -Uri $imageUrl -OutFile $outputPath 
+Invoke-WebRequest -Uri $url -OutFile $outputPath 
 Write-Output "Image downloaded successfully to $outputPath"
 
 # Seteaza imaginea descarcata ca desktop background
