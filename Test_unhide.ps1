e95f658379ff8ad8ac1cd6514a9fe5601f71601c
haiduc29 <ionut.cojocaru93@gmail.com>
2024-06-24 10:44:36 -0700

    Add files via upload

---
+++ Test_unhide.ps1
@@ -0,0 +1,17 @@
+Add-Type -TypeDefinition @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class Taskbar {
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string className, string windowText);
+        [DllImport("user32.dll")]
+        public static extern int ShowWindow(IntPtr hwnd, int command);
+
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 1;
+    }
+"@
+
+$taskbarHwnd = [Taskbar]::FindWindow("Shell_TrayWnd", "")
+[Taskbar]::ShowWindow($taskbarHwnd, [Taskbar]::SW_SHOW)
