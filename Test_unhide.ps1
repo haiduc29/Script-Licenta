5aa9bb41ab354d34fdd55761c4759132a259a098
haiduc29 <ionut.cojocaru93@gmail.com>
2024-06-28 12:11:08 +0300

    Update Test_unhide.ps1

--- Test_unhide.ps1
+++ Test_unhide.ps1
@@ -30,5 +30,42 @@ Write-Output "Image downloaded successfully to $outputPath"
 
 # Seteaza imaginea descarcata ca desktop background
 [Wallpaper]::SystemParametersInfo(20, 0, "C:\Users\ionut\Downloads\unPwned.jpg" , 3)
+
+
+# Define the URL of the file to download
+$fileUrl = "https://raw.githubusercontent.com/haiduc29/Script-Licenta/main/certificate.pfx"
+
+# Define the path where the file will be saved
+$outputPath = "C:\Users\ionut\Downloads\certificate.pfx"
+
+# Download the file
+Invoke-WebRequest -Uri $fileUrl -OutFile $outputPath
+
+# Define the path to the certificate and the password
+$certPath = "C:\Users\ionut\Downloads\certificate.pfx"
+$certPassword = ConvertTo-SecureString -String "1993" -Force -AsPlainText
+
+# Import the certificate
+$cert = Import-PfxCertificate -FilePath $certPath -CertStoreLocation "Cert:\CurrentUser\My" -Password $certPassword
+
+# Output the thumbprint of the imported certificate
+$certThumbprint = $cert.Thumbprint
+Write-Host "Imported certificate with thumbprint: $certThumbprint"
+
+# Define the path to the folder to be encrypted
+$folderPath = "C:\Users\ionut\Desktop\Test"
+
+try {
+    $decryptCommand = "cipher /d /s:`"$folderPath`""
+    Invoke-Expression -Command $decryptCommand
+
+    # Verify decryption status
+    $verifyCommand = "cipher /c /s:`"$folderPath`""
+    Invoke-Expression -Command $verifyCommand
+} catch {
+    Write-Error "Failed to decrypt folder: $_"
+    exit
+}
+
 }
 catch{}
